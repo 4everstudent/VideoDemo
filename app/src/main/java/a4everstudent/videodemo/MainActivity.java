fd73caa22109fb7a88bc597fddfa2af1ebd11c87
4EverStudent <mail@4everstudent.com>
2016-09-23 12:21:51 +0100

    Add video controls.

--- app/src/main/java/a4everstudent/videodemo/MainActivity.java
+++ app/src/main/java/a4everstudent/videodemo/MainActivity.java
@@ -2,6 +2,7 @@ package a4everstudent.videodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
 
         videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
 
         videoView.start();
     }
